356d1dff137f1813f7a04547118df4a56bf83283
Lily Mayer <150291424+lmayer5596@users.noreply.github.com>
2024-04-04 13:02:45 -0400

    final changes

--- project/src/features/build_features.R
+++ project/src/features/build_features.R
@@ -1,7 +1,10 @@
+#read in raw data
 data <- fread('./project/volume/data/raw/data.csv')
 format <- fread('./project/volume/data/raw/example_sub.csv')
 
+#remove id column
 data <- data[, !c('id')]
 
+#save for modelling
 fwrite(data, './project/volume/data/interim/data.csv')
 fwrite(format, './project/volume/data/interim/submit.csv')
